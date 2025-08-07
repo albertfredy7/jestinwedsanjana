673322467037e3ca3434ab1caf87eb52a36157ad
albertfredy7 <albertfredy007@gmail.com>
2025-08-07 13:00:50 +0530

    fix:og image

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,7 +53,7 @@ export default function RootLayout({
           href="favicon/favicon-16x16.png"
         />
         <link rel="manifest" href="favicon/site.webmanifest" />
-        <meta property="og:image" content="https://jestinwedsanjana.vercel.app/gallery-webp/016A7855.webp" />
+        <meta property="og:image" content="https://jestinwedsanjana.vercel.app/gallery-webp/016A7450.webp" />
         <meta property="og:image:type" content="image/webp" />
         <meta property="og:image:width" content="640" />
         <meta property="og:image:height" content="640" />
